42807cb3b0fead9305db60d22aa7a0de975a8311
Janis Heuel <janis-heuel@gmx.de>
2024-08-26 09:16:34 +0200

    playing around with socket and master addr (3)

--- core/utils.py
+++ core/utils.py
@@ -718,24 +718,8 @@ def torch_process_group_init(comm: MPI.Comm, method: str) -> None:
     # # Save environment variables.
     # # os.environ["MASTER_ADDR"] = master_address
     # # Use the default PyTorch port.
-    os.environ["MASTER_PORT"] = str(port)
-
-    # NEW
-    # Get master address and port.
-    # Don't want different groups to use the same port.
-    # subgroup_id = MPI.COMM_WORLD.rank // comm_size
-    # port = 29500 + subgroup_id
-
-    # if comm_size == 1:
-    #     return
-    # master_address = socket.gethostname()
-    # # Each multi-rank worker rank needs to get the hostname of rank 0 of its subgroup.
-    # master_address = comm.bcast(str(master_address), root=0)
-    #
-    # # Save environment variables.
-    # os.environ["MASTER_ADDR"] = master_address
-    # # Use the default PyTorch port.
     # os.environ["MASTER_PORT"] = str(port)
+
 
     if not torch.cuda.is_available():
         method = "gloo"
