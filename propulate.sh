f5b6c02b47a84d403b5c60323e3b502afb26b9b1
Janis Heuel <janis-heuel@gmx.de>
2024-12-05 15:34:34 +0100

    modified shell script to submit job un juwels

--- propulate.sh
+++ propulate.sh
@@ -8,20 +8,24 @@
 #SBATCH --partition=booster
 #SBATCH --gres=gpu:4
 
+
+export "MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n $((SLUMR_PROCID / 4)) )"
+export MASTER_ADDR="$MASTER_ADDR"i
+
 # Load modules
 module purge
 module load GCC OpenMPI
 
 # https://github.com/meta-llama/llama-recipes/issues/65
-export NCCL_P2P_DISABLE=1
+# export NCCL_P2P_DISABLE=1
 # export NCCL_DEBUG=INFO
 
 # Settings by Daniel
-export UCX_MEMTYPE_CACHE=0
-export NCCL_IB_TIMEOUT=100
-export SHARP_COLL_LOG_LEVEL=3
-export OMPI_MCA_coll_hcoll_enable=0
-export NCCL_COLLNET_ENABLE=0
+# export UCX_MEMTYPE_CACHE=0
+# export NCCL_IB_TIMEOUT=100
+# export SHARP_COLL_LOG_LEVEL=3
+# export OMPI_MCA_coll_hcoll_enable=0
+# export NCCL_COLLNET_ENABLE=0
 
 # Activate conda environment
 source /p/home/jusers/heuel1/juwels/micromamba/etc/profile.d/mamba.sh
